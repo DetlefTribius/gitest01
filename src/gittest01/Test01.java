3e69cc8766c14f9cbe9cbfcf09d1560b1d74b2e0
DetlefTribius <detlef.tribius@web.de>
2020-08-11 21:43:45 +0200

    Initial

---
+++ src/gittest01/Test01.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package gittest01;
+
+/**
+ * @author Detlef Tribius
+ *
+ */
+public class Test01
+{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        new Test01();
+    }
+
+}
